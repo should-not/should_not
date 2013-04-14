a9bbfe67103df147c81d6b72a615e6546f9a1d84
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-04-13 17:58:38 -0700

    Dogfood

--- spec/should_not_spec.rb
+++ spec/should_not_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 require 'should_not'
+require 'should_not/rspec'
 
 describe ShouldNot do
-  it 'should have a version number' do
+  it 'has a version number' do
     ShouldNot::VERSION.should_not be_nil
   end
 end
