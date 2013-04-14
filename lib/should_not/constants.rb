85b01102dd274d8b1e9ea9fa0f8adeff4ca5db09
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-04-13 18:13:27 -0700

    Backfill tests for STARTS_WITH_SHOULD

--- lib/should_not/constants.rb
+++ lib/should_not/constants.rb
@@ -1,5 +1,5 @@
 module ShouldNot
-  STARTS_WITH_SHOULD = /\Ashould\s/.freeze
+  STARTS_WITH_SHOULD = /\Ashould(?:n't)?\s/i.freeze
   FAILURE_MESSAGE = "You should_not start an example with should!".freeze
 
   MINITEST_DESCRIPTION_PREFIX = /\Atest_\d+_/.freeze
--- spec/should_not_spec.rb
+++ spec/should_not_spec.rb
@@ -6,4 +6,35 @@ describe ShouldNot do
   it 'has a version number' do
     ShouldNot::VERSION.should_not be_nil
   end
+
+  describe 'constants' do
+    describe 'STARTS_WITH_SHOULD' do
+      subject { ShouldNot::STARTS_WITH_SHOULD }
+
+      it 'matches the whole word "should" at the beginning of the string' do
+        'should foo bar'.should match(subject)
+      end
+
+      it 'matches when "should" is capitalized' do
+        'Should foo bar'.should match(subject)
+      end
+
+      it 'does not match the word "should" in the middle of a string' do
+        'foo should bar'.should_not match(subject)
+      end
+
+      it "matches shouldn't at the beggining" do
+        "shouldn't foo bar".should match(subject)
+      end
+
+      it "does not match shouldn't in the middle" do
+        "foo shouldn't bar".should_not match(subject)
+      end
+
+      it 'does not other words starting with should' do
+        'shoulders are nice to have'.should_not match(subject)
+        'shoulda is a helpful library for testing Rails'.should_not match(subject)
+      end
+    end
+  end
 end
