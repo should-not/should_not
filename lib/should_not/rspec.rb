1404bb47e7f61e2636e625dd794072738ed6d0fc
Martin Gotink <m.gotink@gmail.com>
2014-08-20 21:22:12 +0200

    Fix RSpec 3 compatibility
    RSpec 3 no longer supports the example method, while the new RSpec.current_example method is not available until RSpec 3.0.
    A check is added to support both old and new RSpec version, based on: https://github.com/rspec/rspec-core/blob/347d0289/lib/rspec/core.rb#L133

--- lib/should_not/rspec.rb
+++ lib/should_not/rspec.rb
@@ -2,7 +2,14 @@ require 'rspec/core'
 require 'should_not/constants'
 
 RSpec.configure do |config|
+  # context.example is deprecated, but RSpec.current_example is not
+  # available until RSpec 3.0.
+  fetch_current_example = RSpec.respond_to?(:current_example) ?
+    proc { RSpec.current_example } : proc { |context| context.example }
+
   config.before(:each) do
+    example = fetch_current_example.call(self)
+
     # Not sure that last is correct here -- is the array ever larger than one element?
     description = example.metadata[:description_args].last.to_s
     fail(ShouldNot::FAILURE_MESSAGE) if description.match(ShouldNot::STARTS_WITH_SHOULD)
