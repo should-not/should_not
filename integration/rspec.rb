ddfbd0aaeca68bf90cd4997aa8cf1621bad7dd47
Martin Gotink <m.gotink@gmail.com>
2014-08-20 21:40:10 +0200

    Use Rspec 3 syntax in specs
    The should syntax is deprecated, the new expect syntax has to be used now.

--- integration/rspec.rb
+++ integration/rspec.rb
@@ -2,24 +2,24 @@ require 'should_not/rspec'
 
 describe RSpec do
   it 'should fail when the string starts with should' do
-    1.should == 1
+    expect(1).to eq(1)
   end
 
   it 'can pass a normal test' do
-    1.should == 1
+    expect(1).to eq(1)
   end
 
   it 'does not mind if "should" is in the middle of a string' do
-    1.should == 1
+    expect(1).to eq(1)
   end
 
   describe 'a nested context' do
     it 'should also fail when the string starts with should' do
-      1.should == 1
+      expect(1).to eq(1)
     end
 
     it 'can also pass a normal test' do
-      1.should == 1
+      expect(1).to eq(1)
     end
   end
 end
