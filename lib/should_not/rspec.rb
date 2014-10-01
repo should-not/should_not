5876829ff8a539276cfecf9da48a708bec69893d
Mark Rushakoff <mark.rushakoff@gmail.com>
2014-09-30 22:35:15 -0700

    Convert to RSpec 3

--- lib/should_not/rspec.rb
+++ lib/should_not/rspec.rb
@@ -2,7 +2,7 @@ require 'rspec/core'
 require 'should_not/constants'
 
 RSpec.configure do |config|
-  config.before(:each) do
+  config.before(:each) do |example|
     # Not sure that last is correct here -- is the array ever larger than one element?
     description = example.metadata[:description_args].last.to_s
     fail(ShouldNot::FAILURE_MESSAGE) if description.match(ShouldNot::STARTS_WITH_SHOULD)
--- spec/should_not_spec.rb
+++ spec/should_not_spec.rb
@@ -4,7 +4,7 @@ require 'should_not/rspec'
 
 describe ShouldNot do
   it 'has a version number' do
-    ShouldNot::VERSION.should_not be_nil
+    expect(ShouldNot::VERSION).to_not be_nil
   end
 
   describe 'constants' do
@@ -12,41 +12,42 @@ describe ShouldNot do
       subject { ShouldNot::STARTS_WITH_SHOULD }
 
       it 'matches the whole word "should" at the beginning of the string' do
-        'should foo bar'.should match(subject)
+        expect('should foo bar').to match(subject)
       end
 
       it 'matches when "should" is capitalized' do
-        'Should foo bar'.should match(subject)
+        expect('Should foo bar').to match(subject)
       end
 
       it 'does not match the word "should" in the middle of a string' do
-        'foo should bar'.should_not match(subject)
+        expect('foo should bar').to_not match(subject)
       end
 
       it "matches shouldn't at the beggining" do
-        "shouldn't foo bar".should match(subject)
+        expect("shouldn't foo bar").to match(subject)
       end
 
       it "does not match shouldn't in the middle" do
-        "foo shouldn't bar".should_not match(subject)
+        expect("foo shouldn't bar").to_not match(subject)
       end
 
       it 'does not other words starting with should' do
-        'shoulders are nice to have'.should_not match(subject)
-        'shoulda is a helpful library for testing Rails'.should_not match(subject)
+        expect('shoulders are nice to have').to_not match(subject)
+        expect('shoulda is a helpful library for testing Rails').to_not match(subject)
       end
     end
+
     describe 'MINITEST_DESCRIPTION_PREFIX' do
       subject { ShouldNot::MINITEST_DESCRIPTION_PREFIX }
 
       it 'matches the irrelevant part of the test method name' do
         stripped = 'test_0000_foo bar'.gsub(subject, '')
-        stripped.should == 'foo bar'
+        expect(stripped).to eq('foo bar')
       end
 
       it 'handles when there are more than 10k tests' do
         stripped = 'test_12345_foo bar'.gsub(subject, '')
-        stripped.should == 'foo bar'
+        expect(stripped).to eq('foo bar')
       end
     end
   end
